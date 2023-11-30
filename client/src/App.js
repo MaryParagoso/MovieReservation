aa7195bc72fd722dfae68e49e5ce0bc110165071
deyynnaa <bacoldeannah.aws@gmail.com>
2023-11-30 21:04:48 +0800

    few debugs

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Login from "./pages/Login";
-import Registration from './pages/Registration';
+import Registration from "./pages/Registration";
 import MenuPage from "./pages/MenuPage";
 import SeatLayout from "./pages/SeatLayout";
 import Movielist from "./pages/Movielist";
@@ -18,7 +18,10 @@ const App = () => {
         <Route path="/menu" element={<MenuPage />}></Route>
         <Route path="/seat-layout" element={<SeatLayout />}></Route>
         <Route path="/movielist" element={<Movielist />}></Route>
-        <Route path="/confirmation" element={<ReservationConfirmation />}></Route>
+        <Route
+          path="/confirmation"
+          element={<ReservationConfirmation />}
+        ></Route>
       </Routes>
     </Router>
   );
