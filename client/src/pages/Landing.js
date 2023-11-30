ac80698587628fc8666ae6785846a03fed9ed45e
deyynnaa <bacoldeannah.aws@gmail.com>
2023-11-30 21:09:07 +0800

    ignore this

--- client/src/pages/Landing.js
+++ client/src/pages/Landing.js
@@ -24,7 +24,7 @@ function Landing() {
           </Breadcrumb>
           <div style={centerContent}>
             <h1 style={Context1}>Welcome to Our Website</h1>
-            <p style={Context2}>Explore amazing content and more!</p>
+            <p style={Context2}>Explore amazing content and more!!</p>
             <div>
               <Button ghost size="large" style={buttonLogin}>
                 Log In
