71b38612dbb2785eef8224a82756a3cad1c66440
Seth Angelo Llenos <seth.llenos@awsys-i.com>
2023-12-01 18:20:14 +0800

    Back-end have some improvements

--- server/config/dbConfig.js
+++ server/config/dbConfig.js
@@ -1,8 +1,26 @@
-const mongoose = require("mongoose");
-mongoose.connect(process.env.mongo_url);
+// config/dbConfig.js
+const mongoose = require('mongoose');
+const Movie = require('../models/Movie');
+
+// // Load environment variables from .env file
+// require('dotenv').config();
+
+// // Use process.env.MONGO_URL to get the MongoDB connection string
+// mongoose.connect(process.env.MONGO_URL, {
+//   useNewUrlParser: true,
+//   useUnifiedTopology: true,
+//   //useCreateIndex: true, // Add this line
+// });
+
+mongoose.connect("mongodb+srv://bacoldeannahaws:aDdF5EGN9luxZFfz@cluster0.pbdtb2q.mongodb.net/MovieReservation", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  //useCreateIndex: true, // Add this line
+});
+
 const connection = mongoose.connection;
 
-connection.on("connected", () =>
-  console.log("mongooDB is connected successfully")
-);
-connection.on("error", () => console.log("mongooDB failed"));
+connection.on('connected', () => console.log('MongoDB connected successfully'));
+connection.on('error', (err) => console.error('MongoDB connection error:', err));
+
+module.exports = connection;
